4f2d95dbd98895a769fb91723624eb8f774d4d90
mesadhan <sadhansarker3@gmail.com>
2019-07-03 14:05:27 +0600

    updated app logo issue

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,24 +1,31 @@
 import Route from '@ember/routing/route';
 import {inject as service} from '@ember/service';
-
+import config from 'frontend-engine/config/environment';
 
 import Authorization from '../mixins/authorization'
 
 
 export default Route.extend(Authorization, {
 
+  appLogo: config.APP.appLogo,
   appConfiguration: service('app-configuration'),
   appRestTemplate: service('app-rest-template'),
+  model(service_data, transition) {
+
+    return {
+      appLogo: this.appLogo
+    }
+  },
   beforeModel() {
 
     if (this.isAuthorization()) {
       this.transitionTo("welcome");
     }
-
   },
 
-  setupController(controller, model) {
+  /*setupController(controller, model) {
     console.log('message', 'setupController ', model);
-  },
+    this.set('appLogo', this.appLogo);
+  },*/
 
 });
