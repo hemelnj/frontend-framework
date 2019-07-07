a4cf4bdd69f0c5a483aa90a15c701cd85654cae0
mukitul <mukitulislamratul@gmail.com>
2019-07-07 18:01:23 +0600

    need to update p-e

--- app/components/nges-engines/property-extender/add-property.js
+++ app/components/nges-engines/property-extender/add-property.js
@@ -10,33 +10,58 @@ export default Component.extend({
   peSetupService: service('nges-engines/property-extender/pe-setup'),
 
   peFormData: {
-    string:{
-      name:'',
-      id:''
-    },
+    stringName:'',
+    stringId:'',
   },
 
   notifier: service(),
 
   init() {
     this._super(...arguments);
+    this.set('attribute', []);
   },
 
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('attributes',this.get('attributeList'));
+  },
 
   actions: {
 
+
     addAction() {
 
-      let string = this.get('peFormData.string.name');
-      let stringId = this.get('peFormData.string.id');
 
-      console.log('message-peFormData-name', string,stringId);
+      let attributes = this.get('attributes');
+      console.log('message--attributes', attributes.result);
 
 
-      let payload = [{
+      let context = this;
 
-      }];
+      for (let i=0;i<attributes.result.length;i++) {
+        let record = {
+          id:attributes.result[i].id,
+          type:attributes.result[i].type,
+          instanceId:attributes.result[i].classTypeId,
+          value: attributes.result[i].name,
+          code: 'n/a',
+          describtion: 'n/a',
+          createdBy: 'msi',
+          createdAt:1,
+          lastUpdatedAt:1,
+          lastUpdatedBy:'msi',
+          comments:'n/a',
+          extra: 'n/a',
+          name:"n/a",
+          attribute: {
+            id:attributes.result[i].id,
+          }
+        };
 
+        context.get('attribute').pushObject(record);
+      }
+
+      let payload = this.get('attribute');
       console.log('message', payload);
 
       /*let accessToken = this.appConfiguration.getAccessToken();
