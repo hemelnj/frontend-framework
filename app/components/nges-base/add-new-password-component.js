6cb3ea6727ca226838cd15b1e8b3b26e71375b10
mukitul <mukitulislamratul@gmail.com>
2019-07-24 15:29:02 +0600

    forgot-password issue fixed

--- app/components/nges-base/add-new-password-component.js
+++ app/components/nges-base/add-new-password-component.js
@@ -8,40 +8,10 @@ export default Component.extend({
   appLogin: service('nges-core/app-login'),
   router: service(),
   errorMsg: '',
+  notifier: service(),
   init() {
     this._super(...arguments);
   },
-
-  changePasswordRequest(userData) {
-
-
-    let context = this;
-
-
-    /*$.ajax({
-      type: "POST",
-      url: context.authEngineHost+"/users/passwordRecover",
-
-      data: JSON.stringify(userData),
-      dataType: "json",
-
-      beforeSend: function (xhr) {
-        xhr.setRequestHeader('content-type', 'application/json');
-      },
-
-      statusCode: {
-        400: function (msg) {
-          console.log('message-post-data', 'update Password failure');
-          context.set('errorMsg',msg.responseJSON.message);
-        },
-        200:function(){
-          console.log('message-post-data', 'successful pass req');
-          context.get('router').transitionTo('login');
-        }
-      },
-    });*/
-  },
-
 
   actions: {
     updatePassword(newPassword, rePassword) {
@@ -64,10 +34,11 @@ export default Component.extend({
         this.appLogin.updatePasswordAfterGettingCode(userData).then(function (result) {
 
         }).catch(function (msg) {
-          if(msg.status === 200){
+          if (msg.status === 200) {
+            context.get('notifier').success('Password Updated!');
             context.get('router').transitionTo('login');
           }
-          context.set('errorMsg',JSON.parse(msg.responseText).message);
+          context.set('errorMsg', JSON.parse(msg.responseText).message);
         })
 
       } else {
