50b6d8fc1f10ce866153b6a73507fcba3259f961
mukitul <mukitulislamratul@gmail.com>
2019-09-05 09:36:29 +0600

    working on rms-core

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -15,8 +15,8 @@ export default Controller.extend(Authorization, {
     this._super(...arguments);
 
     this.set('userInformation', {
-      name: 'SI',
-      password: 'password'
+      name: 'shahidul',
+      password: '1\\Z-.^E0'
     })
   },
   actions: {
