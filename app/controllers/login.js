8b558db3908f255ff990c2319f2fb51176899902
mukitul <mukitulislamratul@gmail.com>
2019-09-15 19:15:26 +0600

    login ui input modified

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -15,8 +15,8 @@ export default Controller.extend(Authorization, {
     this._super(...arguments);
 
     this.set('userInformation', {
-      name: 'msi_inputter',
-      password: 'password'
+      name: '',
+      password: ''
     })
   },
   actions: {
