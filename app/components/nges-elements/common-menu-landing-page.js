426d8751d4ec7a334431629435f918885e8cf83e
mukitul <mukitulislamratul@gmail.com>
2019-09-18 10:06:57 +0600

    update in menu-landing-page

--- app/components/nges-elements/common-menu-landing-page.js
+++ app/components/nges-elements/common-menu-landing-page.js
@@ -12,12 +12,33 @@ export default Component.extend({
   ngesTabTableService: service('nges-elements/nges-tab-table'),
   serviceInitializer: service('nges-services/service-initializer'),
   notifier: service(),
+  router: service(),
+  routePath:"welcome.application-loader.panel-loader.module-loader.service-holder-loader.menu-template-loader.submenu-template-loader.submenu-detail-template-loader",
 
   init() {
     this._super(...arguments);
 
     let routeInfo = this.get('routeInformation');
-    console.log('messager-----------outeInformationrouteInformation', routeInfo);
+    console.log('messager-----------outeInformationrouteInformation', routeInfo.completeTemplateName);
   },
+
+  actions:{
+    newView(){
+      console.log('message-newView-click');
+      let context = this;
+      context.get("router").transitionTo(this.routePath);
+    },
+
+    searchView(){
+
+    },
+
+    gridView(){
+      console.log('message-gridView-click');
+      let routeInfo = this.get('routeInformation');
+      let context = this;
+      context.get("router").transitionTo(this.routePath,routeInfo.completeTemplateName);
+    },
+  }
 
 })
