0a6c803ee1d00e492b0b204ce15f9ed01464cf6c
mukitul <mukitulislamratul@gmail.com>
2019-09-23 13:04:06 +0600

    working on common-search grid

---
+++ app/components/nges-elements/search-common-grid.js
@@ -0,0 +1,276 @@
+import Component from '@ember/component';
+import {inject as service} from '@ember/service';
+
+let DEFAULT_SELECTED_TAB_CODE = 'allStates';
+
+
+export default Component.extend({
+
+  appWelcome: service('nges-core/app-welcome'),
+  store: service(),
+  appConfiguration: service('app-configuration'),
+  ngesTabTableService: service('nges-elements/nges-tab-table'),
+  serviceInitializer: service('nges-services/service-initializer'),
+  notifier: service(),
+
+  init() {
+    this._super(...arguments);
+    this.createButtonAccess();
+    this.getDefaultUserFunctionId();
+    this.getDefaultUserLocationId();
+  },
+
+  createButtonAccess() {
+    let context = this;
+    let roleId = this.appConfiguration.getUserRoleId();
+    let accessToken = this.appConfiguration.getAccessToken();
+
+    this.serviceInitializer.getClassType(accessToken).then(function (result) {
+      let classTypeId = result.data;
+      context.serviceInitializer.getUserAbilityToCreate(accessToken, "create", classTypeId, roleId).then(function (result) {
+        try {
+          if (result.status === 204) {
+            context.set("viewCreateButton", false);
+          } else {
+            context.set("viewCreateButton", true);
+          }
+        } catch (e) {
+        }
+      });
+    });
+  },
+
+
+  getDefaultUserFunctionId() {
+    let context = this;
+    let accessToken = this.appConfiguration.getAccessToken();
+    let userId = this.appConfiguration.getUserId();
+    let orgId = this.appConfiguration.getOrganizationId();
+    let functionId = this.serviceInitializer.getDefaultFunctionId(accessToken, userId, orgId);
+
+    functionId.then(function (msg) {
+      console.log('message--functionId', msg.data.attributes.id);
+      context.set('defaultFunction', msg.data.attributes.id);
+    });
+  },
+  getDefaultUserLocationId() {
+    let context = this;
+    let accessToken = this.appConfiguration.getAccessToken();
+    let userId = this.appConfiguration.getUserId();
+    let orgId = this.appConfiguration.getOrganizationId();
+    let locationId = this.serviceInitializer.getDefaultLocationId(accessToken, userId, orgId);
+
+    locationId.then(function (msg) {
+      console.log('message--locationId', msg.data.attributes.id);
+      context.set('defaultLocation', msg.data.attributes.id);
+    }).catch(function (errorMsg) {
+      context.get('notifier').danger('Failed to Load User Default Location Id');
+    });
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+
+    this.initializeTabTableMeta();     // tab table initialize only once
+
+    this.initialLoadTabTableData();
+  },
+  actions: {
+    // callback call from item
+    onSelectedSingleStateCallBack(item, actionEventId, stateId) {
+      console.log('onSelectedSingleState-Item', item);
+      console.log('onSelectedSingleState-actionEventId', actionEventId);
+      console.log('onSelectedSingleState-stateId', stateId);
+
+      //console.log('message-onSelectedSingleStateCallBack', JSON.stringify(payload));
+      this.performActionEvent(item, actionEventId, stateId);
+    },
+
+    onSelectedMultipleStateCallBack(actionEventId) {
+      console.log('onSelectedMultipleState-Items', actionEventId);
+    },
+
+    onSelectedTabCallBack(tab) {
+      console.log('onSelectedTabCallBack-Items', tab);
+      this.editPermissionAccess(tab);
+    },
+  },
+
+  initialLoadTabTableData() {
+    let context = this;
+
+    let defaultFunction = this.get('defaultFunction');
+    let defaultLocation = this.get('defaultLocation');
+
+
+
+    let accessToken = context.appConfiguration.getAccessToken();
+    let roleList = context.appConfiguration.getUserRoleIdList();
+    let stateList = context.getStateListIds();
+
+    // request to the server to get table data
+    this.ngesTabTableService.loadTabTableData(
+      defaultFunction,
+      defaultLocation,
+      roleList,
+      stateList,
+      accessToken
+    ).then(function (result) {
+      //console.log('message', JSON.stringify(result));
+
+      context.set('tableData', result);           // initialize tab table headers & table data
+
+    }, function (error) {
+      console.log('message', error);
+    }).then(function () {
+      //console.log('message', 'need to load States');   // todo: need to load States
+    });
+  },
+
+  getStateListIds() {
+    let stateList = [];
+    //let states = this.get('stateList');
+    let states = this.appWelcome.getSateList();
+
+    for (let i = 0; i < states.length; i++) {
+      stateList.push(states[i].id)
+    }
+    return stateList;
+  },
+
+  initializeTabTableMeta() {
+
+
+    // initialize stateList
+    let stateList = this.appWelcome.getSateList();
+    console.log('message--states', stateList);
+
+    if(stateList.length >= 2){
+
+
+      // selected default active tab
+      let activeTabInfo = {
+        'id': stateList[1].id,
+        'name': stateList[1].name,
+        'code': stateList[1].code,
+      };
+
+
+
+      /*
+      stateList.unshift({
+        'id': 11,
+        'name': 'All',
+        'code': DEFAULT_SELECTED_TAB_CODE,            //
+        'tabContentClass': 'active',   // fade
+        'tabTitleClass': 'active'       //
+      });
+
+      // selected default active tab
+      let activeTabInfo = {
+        'id': '00',
+        'name': 'All',
+        'code': DEFAULT_SELECTED_TAB_CODE,
+      };*/
+
+      this.set('stateActions', []);     // default stateActions
+      this.set('stateList', stateList);
+      this.set('activeTabInfo', activeTabInfo);
+    }
+
+
+
+  },
+
+  performActionEvent(item, actionEventId, stateId) {
+
+    let accessToken = this.appConfiguration.getAccessToken();
+    let roleId = this.appConfiguration.getUserRoleId();
+
+    let context = this;
+    context.serviceInitializer.getClassType(accessToken).then(function (result) {
+      let classTypeId = result.data;
+
+      let getNextAllowableStatePayload = {
+        classTypeId: classTypeId,
+        stateId: stateId,
+        roleId: roleId,
+        actionEventId: actionEventId
+      };
+
+      context.serviceInitializer.getNextAllowableState(accessToken, getNextAllowableStatePayload).then(function (result) {
+        try {
+          let status = result.data.id;
+          let itemId = item.id;
+          let payload = {
+            data: {
+              type: item.type,
+              attributes: {
+                //status: status
+                olcmState: {
+                  id: status,
+                },
+              }
+            }
+          };
+          context.serviceInitializer.stateActionUpdate(accessToken, itemId, payload).then(function (result) {
+            context.get('notifier').success('Successfully Send to Next State');
+          })
+        } catch (e) {
+          console.error('message', 'GetNextAllowableState Not Found');
+          context.get('notifier').danger('GetNextAllowableState Not Found');
+        }
+      });
+    });
+  },
+
+
+  editPermissionAccess(tab) {
+    let context = this;
+    let roleId = this.appConfiguration.getUserRoleId();
+    let accessToken = this.appConfiguration.getAccessToken();
+    let stateId = tab.id;
+
+
+    this.serviceInitializer.getClassType(accessToken).then(function (result) {
+      let classTypeId = result.data;
+
+      let data = {
+        classTypeId: classTypeId,
+        roleId: roleId,
+        stateId: stateId,
+      };
+      context.serviceInitializer.getAllAllowableCrudAction(accessToken, data).then(function (result) {
+        try {
+          console.log('message--result', result);
+          if (result.status === 204) {
+            context.set("actionAccess", {
+              view: false,
+              edit: false,
+            });
+          } else {
+
+
+            let permission = (result) => {
+              let data = {};
+              for (let access of result.data) {
+                if (access.attributes.name.toLowerCase() === "view") {
+                  data['view'] = true;
+                }
+                if (access.attributes.name.toLowerCase() === "edit") {
+                  data['edit'] = true;
+                }
+              }
+              return data
+            };
+
+            context.set("actionAccess", permission(result));
+          }
+        } catch (e) {
+        }
+      });
+    });
+  },
+
+})
