65f4713a24fa4900967b4fbad1076d6f38f30383
mukitul <mukitulislamratul@gmail.com>
2019-10-17 11:02:36 +0600

    language setting ui created--- need to update

--- app/components/nges-elements/system-language-setting.js
+++ app/components/nges-elements/system-language-setting.js
@@ -11,26 +11,32 @@ export default Component.extend({
   ngesTabTableService: service('nges-elements/nges-tab-table'),
   serviceInitializer: service('nges-services/service-initializer'),
   notifier: service(),
+  intl: service(),
+
+
+  selectedLanguage: null,
 
   init() {
     this._super(...arguments);
     this.loadLanguage();
   },
 
-  loadLanguage(){
+  loadLanguage() {
     let data = [
       {
         id: 1,
         type: "language",
         attributes: {
-          name: "Bengali"
+          name: "Bengali",
+          fileName: "bn-ln"
         }
       },
       {
         id: 2,
         type: "language",
         attributes: {
-          name: "English"
+          name: "English",
+          fileName: "en-us"
         }
       }
     ];
@@ -38,12 +44,26 @@ export default Component.extend({
     this.set("languageList", data);
   },
   actions: {
-    onSelectLanguage(value){
-      console.log('message-onSelectLanguage', value.attributes.name);
+    onSelectLanguage(value) {
+
+      let language = value.attributes.fileName;
+
+      this.set('selectedLanguage', value.attributes.name);
+
+      this.appConfiguration.setAppLanguage(language);
     },
 
-    save(){
-      console.log('message--save--action');
+    save() {
+      let language = this.get('selectedLanguage');
+
+      if (language != null) {
+        this.get('notifier').success('Language Changed Successfully');
+        this.set('selectedLanguage', null);
+      } else {
+        this.get('notifier').danger('Failed To Change Language');
+        this.set('selectedLanguage', null);
+      }
+
     }
   }
 
